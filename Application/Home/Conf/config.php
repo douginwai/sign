d55c7fabfa42bf2ec00bb5565834e3b40565f163
douginwai <379980437@qq.com>
2019-03-21 10:29:35 +0800

    Update config.php

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -12,14 +12,6 @@ return array(
     'DOMAIN_URL' => 'http://localhost/signin',
 
     //'配置项'=>'配置值'
-//    'DB_TYPE'   => 'mysql', // 数据库类型
-//    'DB_HOST'   => 'w.rdc.sae.sina.com.cn', // 服务器地址
-//    'DB_NAME'   => 'app_qrsigin', // 数据库名
-//    'DB_USER'   => 'wjl0o5nonl', // 用户名
-//    'DB_PWD'    => 'xkw35kjkj4w1y324wik10mzl2545zyj0lwj0w21x', // 密码
-//    'DB_PORT'   => 3306, // 端口
-//    'DB_PREFIX' => 'tb_', // 数据库表前缀
-//    'DB_CHARSET'=> 'utf8', // 字符集
-//    'DOMAIN_URL' => 'http://qrsigin.applinzi.com',
+
 
 );
